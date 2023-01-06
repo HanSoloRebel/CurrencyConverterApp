6bb10a492db697bed51bf2aeda0fc8acd7d1a46b
HanSoloRebel <bedin86@gmail.com>
2023-01-06 19:38:20 +0400

    20230106 add CurrencyConverter/currencyDictionary

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -5,7 +5,15 @@ public class CurrencyConverter {
         private static float exchangeRate = -1;
         private static float amount = -1;
         private static String[] exchangeCurrencies;
-        private static List<String> currenciesList = new ArrayList<>(asList("RUB", "CNY", "KZT", "BYN","TRY", "USD","UER"));
+        private static Map<String, String[]> currencyDictionary = new HashMap<>(){{
+            put("RUB", new String[] {"Российский рубль", "Российского рубля"});
+            put("CNY", new String[] {"Китайский юань", "Китайского юаня"});
+            put("KZT", new String[] {"Казахстанский тенге", "Казахстанского тенге"});
+            put("BYN", new String[] {"Белорусский рубль", "Беларусского рубля"});
+            put("TRY", new String[] {"Турецкая лира", "Турецкой лиры"});
+            put("USD", new String[] {"Доллар США", "Доллара США"});
+            put("UER", new String[] {"Евро", "Евро"});
+        }};
 
         public static void setExchangeRate(float rate) {
             if (rate > 0) {
@@ -23,28 +31,28 @@ public class CurrencyConverter {
                 System.out.println(errorCode);
             }
         }
-    public static void setExchangeCurrencies(String[] currencies){
-        boolean valueError = false;
-        if (currencies.length == 2){
-            for (String i : currencies){
-                if (!currenciesList.contains(i)){
-                    String errorCode1 = String.format("ValueError: the %s is not in currency list.", i);
-                    System.out.println(errorCode1);
-                    valueError = true;
+        public static void setExchangeCurrencies(String[] currencies){
+            boolean valueError = false;
+            if (currencies.length == 2){
+                for (String i : currencies){
+                    if (!(currencyDictionary.keySet()).contains(i)){
+                        String errorCode1 = String.format("ValueError: the %s is not in currency list.", i);
+                        System.out.println(errorCode1);
+                        valueError = true;
+                    }
                 }
+                if (!valueError){
+                    exchangeCurrencies = currencies;
+                }
+            } else {
+                String errorCode2 = String.format("ValueError: two  currency values must be passed.");
+                System.out.println(errorCode2);
             }
-            if (!valueError){
-                exchangeCurrencies = currencies;
-            }
-        } else {
-            String errorCode2 = String.format("ValueError: two  currency values must be passed.");
-            System.out.println(errorCode2);
         }
-    }
         public static String getExchange(){
             if ((exchangeRate != -1) &&  (amount != -1) && (exchangeCurrencies.length == 2)) {
                 float result = amount * exchangeRate;
-                System.out.printf("1 %s = %s %s\nЦБ РФ %s\n", exchangeCurrencies[0], exchangeRate, exchangeCurrencies[1], UserInterface.getDate());
+                System.out.printf("1 %s = %s %s\nЦБ РФ %s\n", currencyDictionary.get(exchangeCurrencies[0])[0], exchangeRate, currencyDictionary.get(exchangeCurrencies[1])[1], UserInterface.getDate());
                 String str = String.format("%s %s = %s %s", amount, exchangeCurrencies[0], result, exchangeCurrencies[1]);
                 return str;
             } else {
