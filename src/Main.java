4fa1e42b4c63e170fec860ee3e14c9f023b0105a
Alexey <79377808+HanSoloRebel@users.noreply.github.com>
2023-01-06 19:35:35 +0300

    Update Main.java
    edit currencies

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         float rate = 6.5f;
         float value = 100;
-        String[] currencies = new String[] {"KZT"};
+        String[] currencies = new String[] {"RUB", "KZT"};
         CurrencyConverter exchange = new CurrencyConverter();
         exchange.setExchangeRate(rate);
         exchange.setAmount(value);
